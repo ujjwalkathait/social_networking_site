7d8811953c60e20ae01e19fc80868153b9bc4474
ujjwalkathait <kathaitujjwal@gmail.com>
2023-11-25 02:02:16 +0530

    GUUUUU

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,51 @@
-import AuthLayout from './_auth/AuthLayout';
-import SigninForm from './_auth/forms/SigninForm';
-import SignupForm from './_auth/forms/SignupForm';
-import RootLayout from './_root/RootLayout';
-import { Home } from './_root/pages';
+import { Routes, Route } from "react-router-dom";
 
-import './globals.css';
-import {Routes, Route} from 'react-router-dom';
+import {
+  Home,
+  Explore,
+  Saved,
+  CreatePost,
+  Profile,
+  EditPost,
+  PostDetails,
+  UpdateProfile,
+  AllUsers,
+} from "@/_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import SignupForm from "@/_auth/forms/SignupForm";
+import SigninForm from "@/_auth/forms/SigninForm";
+import { Toaster } from "@/components/ui/toaster";
+
+import "./globals.css";
 
 const App = () => {
   return (
-    <main className='flex h-screen'>
-        <Routes>
-          <Route element={<AuthLayout />}>
-            <Route path='/sign-in' element={<SigninForm />}/>
-            <Route path='/sign-up' element={<SignupForm />}/>
-          </Route>
-        
-          <Route element={<RootLayout/>}>
-            <Route index element = {<Home />}/>
-          </Route>
-        </Routes>
-    </main>
-  )
-}
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
 
-export default App
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
+        </Route>
+      </Routes>
+
+      <Toaster />
+    </main>
+  );
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
-import ReactDOm from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
+import { AuthProvider } from "@/context/AuthContext";
+import { QueryProvider } from "@/lib/react-query/QueryProvider";
+
 import App from "./App";
 
-ReactDOm.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <BrowserRouter>
-        <App />
+      <QueryProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryProvider>
     </BrowserRouter>
-)
+  </React.StrictMode>
+);
