bed1e546abb56baffb1ebb8162187177efefa435
ujjwalkathait <kathaitujjwal@gmail.com>
2023-11-25 01:52:52 +0530

    first commit

---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,17 @@
+// import * as z from "zod"
+
+import { Button } from "@/components/ui/button";
+ 
+// const formSchema = z.object({
+//   username: z.string().min(2).max(50),
+// })
+
+const SignupForm = () => {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default SignupForm
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import ReactDOm from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+ReactDOm.createRoot(document.getElementById('root')!).render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+)
